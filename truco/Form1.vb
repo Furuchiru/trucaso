302e3d28dec04a57447a797b231a99595b2f23c0
Furuchiru <elchapi5000@gmail.com>
2019-04-29 18:28:39 -0300

    random string

--- truco/Form1.vb
+++ truco/Form1.vb
@@ -1,13 +1,18 @@
-﻿Public Class Form1
+﻿Imports System.Text
+Public Class Form1
+
+
+    Public Shared Function randomString()
+        Dim r As New Random
+        Dim s As String = "BECO"
+        Dim sb As New StringBuilder
+        Dim idx As Integer = r.Next(0, 4)
+        sb.Append(s.Substring(idx, 1))
+        Return sb.ToString()
+    End Function
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
-
-
-
-        'wachin
-    End Sub
-
-    Sub algo()
-
+        MsgBox(randomString())
     End Sub
 End Class
